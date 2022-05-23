22a7cd244ce1597f36e68ecf46dad6727c78e6b7
Farhan Tahmid <farhantahmid881@gmail.com>
2022-05-23 19:02:36 +0600

    Logics of Assistant

--- boss.py
+++ boss.py
@@ -1,6 +1,10 @@
+import imp
+from unittest import result
 import pyttsx3
 import datetime
 import speech_recognition as speech
+import wikipedia
+import webbrowser
 
 engine=pyttsx3.init('sapi5') #API for microsoft speech
 voices=engine.getProperty('voices')
@@ -9,7 +13,10 @@ engine.setProperty('voice',voices[0].id)
 
 def audio(sound): #generate computer audio
     engine.say(sound)
+    print(sound)
     engine.runAndWait()
+
+
 def listeningCommands(): #listens to users and shows what is told in text
     recognizer=speech.Recognizer()
     with speech.Microphone() as source:
@@ -23,7 +30,7 @@ def listeningCommands(): #listens to users and shows what is told in text
         print(f"{query}")
     except Exception as exc:
         #print(exc)
-        audio("Can you repeat please?")
+        #audio("Can you repeat please?")
         print("Can you repeat please?")
         return "None" 
     return query
@@ -37,6 +44,39 @@ def greetings():
     else:
         audio("Good Evening sir!")
     audio("This is the boss here. How may i help you, sir?")
+
+
 if __name__=="__main__":
     greetings()
-    listeningCommands()
+    while True:
+        query=listeningCommands().lower()
+        #executing tasks
+        if 'wikipedia' in query:
+            audio("Boss is searching from wikipedia...")
+            query=query.replace("wikipedia","")
+            results=wikipedia.summary(query,sentences=2)
+            audio("According to wikipedia")
+            print(results)
+            audio(results)
+        elif 'open youtube' in query:
+            audio("Opening Youtube!")
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            audio("Opening google")
+            webbrowser.open("google.com")
+        elif 'search in google' in query:
+            audio("Searching in google..")
+            query=query.replace("search in google","")
+            webbrowser.open(f"{query}")
+        elif 'open stackoverflow' in query:
+            audio("Opening stack overflow")
+            webbrowser.open("stackoverflow.com")
+        elif 'in stackoverflow' in query:
+            query=query.replace("in stackoverflow","")
+            audio(f"{query} opening in stack overflow..")
+            webbrowser.open(f"stackoverflow.com/{query}")
+        elif 'facebook' in query:
+            audio("Opening facebook")
+            webbrowser.open("facebook.com") 
+        
+
